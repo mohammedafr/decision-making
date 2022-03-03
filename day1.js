7186dec489518c1abc4b23542e17214647c454f3
mohammedafr <mohammedafridahmed1980@gmail.com>
2022-03-03 20:36:56 +0530

    Hi

---
+++ day1.js
@@ -0,0 +1,99 @@
+//  //Decision Making
+
+// if(condtion){
+//   sucesss
+// }
+// else{
+//   failure
+// } 
+
+
+//  //if-else ladder
+
+// let a= -10;
+// if(a<0){
+//   console.log("Negative");
+// }
+// else if(a==0){
+//   console.log("Zero");
+// }
+// else{
+//   console.log("Positive");
+// }
+
+
+// //Three Or More Conditions
+// //Switch statement
+
+// let gender="m";
+// switch(gender){
+//   case "m" :
+//      console.log("MALE");
+//      break;
+//   case "f" :
+//      console.log("FEMALE");
+//      break;
+//   default :
+//   console.log("TRANSGENDER");
+//   break;
+// }
+
+
+// let age = 35;
+
+// switch(true){
+//   case "age<18" : 
+//     console.log("Child");
+//      break;
+//   case "18<age<40" :
+//   console.log("Adult");
+//      break;
+//   default:
+//      console.log("Senior");
+//         break;
+// }
+
+
+//varible day
+
+// let day=1;
+// switch(day){
+//   case 0 :
+//   console.log("Monday");
+//   break;
+//   case 1 :
+//   console.log("Tuesday");
+//   break;
+//   case 2 :
+//   console.log("Wednesday");
+//   break;
+//   case 3 :
+//   console.log("Thursday");
+//   break;
+//  case 4 :
+//   console.log("Friday");
+//   break;
+//   case 5 :
+//   console.log("Saturday");
+//   break;
+//   default:
+//   console.log("Sunday");
+//   break;
+// }
+
+
+//Loops And Iterations
+//Iterative Statements
+
+//Loops - For ,  While , Do-while
+
+//3 values
+// Initialization
+// Finalization
+// Decrement/ Increment
+
+
+for(let i=1;i<=10;i++){
+  var mul = 19*i;
+  console.log("19 * " +i +" = " + 19*i);
+}
